f8a364c8a623ac41067fa9a3e5f4a739d4aa0066
unknown <Italo.Adler@digitalcollege.internal>
2024-11-05 19:54:07 -0300

    Client sends messages to server

--- index.js
+++ index.js
@@ -2,9 +2,25 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>');
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    
+    // Lidar com a mensagem de chat enviada pelo cliente
+    socket.on('chat message', (msg) => {
+      console.log('message: ' + msg);
+    });
+    
+    // Lidar com a desconexão do cliente
+    socket.on('disconnect', () => {
+      console.log('user disconnected');
+    });
 });
 
 server.listen(3000, () => {
